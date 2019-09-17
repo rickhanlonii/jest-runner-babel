cfe539b97a7e502813cdd2cc4241bd47bef94f4f
Charlike Mike Reagent <opensource@tunnckocore.com>
2019-09-17 17:17:56 +0300

    feat: refactor to support monorepos
    Signed-off-by: Charlike Mike Reagent <opensource@tunnckocore.com>

--- run.js
+++ run.js
@@ -12,25 +12,84 @@ module.exports = ({ testPath, config }) => {
     .load(config.rootDir)
     .then(explorer => {
       const runnerConfig = explorer.config;
-      const result = babel.transformFileSync(
-        testPath,
-        runnerConfig.babel || {}
-      );
 
-      if (runnerConfig.outDir) {
-        // LMAO do this smarter
-        const testDir = path.dirname(testPath);
-        const outFile = testPath.replace(config.rootDir, runnerConfig.outDir);
-        const outPath = testDir.replace(config.rootDir, runnerConfig.outDir);
+      let result = null;
 
-        mkdirp.sync(outPath);
-        fs.writeFileSync(outFile, result.code);
+      try {
+        result = transformFileSync(testPath, runnerConfig);
+      } catch (err) {
+        return fail({
+          start,
+          end: new Date(),
+          test: { path: testPath, title: 'Babel', errorMessage: err.message },
+        });
       }
+
+      // Classics in the genre! Yes, it's possible, sometimes.
+      // Old habit for ensurance
+      if (!result) {
+        return fail({
+          start,
+          end: new Date(),
+          test: {
+            path: testPath,
+            title: 'Babel',
+            errorMessage: 'Babel failing to transform...',
+          },
+        });
+      }
+
+      runnerConfig.outDir = runnerConfig.outDir || runnerConfig.outdir;
+
+      if (typeof runnerConfig.outDir !== 'string') {
+        runnerConfig.outDir = 'dist';
+      }
+
+      let { rootDir } = config;
+      let relativeTestPath = path.relative(rootDir, testPath);
+
+      // console.log(testPath);
+      // we are in monorepo environment,
+      // so make dist folder in each package root
+      if (isMonorepo(config.cwd)) {
+        if (isWin32 && !relativeTestPath.includes('/')) {
+          relativeTestPath = relativeTestPath.replace(/\\/g, '/');
+        }
+
+        const segments = relativeTestPath.split('/');
+        while (segments.length > 3) segments.pop();
+
+        rootDir = path.join(rootDir, ...segments);
+
+        // common case
+        if (rootDir.endsWith('src')) {
+          relativeTestPath = path.relative(rootDir, testPath);
+          rootDir = path.dirname(rootDir);
+        }
+        // ! important: the `rootDir` may be package's directory
+        // ! like <monorepo root>/<workspace name>/<package dir> so we can append the dist
+      }
+
+      // common case
+      if (relativeTestPath.startsWith('src')) {
+        relativeTestPath = path.relative(path.join(rootDir, 'src'), testPath);
+      }
+
+      let outDir = path.resolve(rootDir, runnerConfig.outDir);
+      let outFile = path.join(outDir, relativeTestPath);
+
+      // the new dir of the new file (may include some path from the testPath)
+      outDir = path.dirname(outFile);
+
+      const testFile = path.basename(outFile, path.extname(outFile));
+
+      fs.mkdirSync(outDir, { recursive: true });
+      fs.writeFileSync(path.join(outDir, `${testFile}.js`), result.code);
 
       return pass({
         start,
         end: new Date(),
-        test: { path: testPath },
+        test: { path: outFile },
       });
     })
     .catch(parsingError => {
